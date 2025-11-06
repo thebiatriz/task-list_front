98f6063cbdfeb8b81fbfd7cab4a5c5f110251437
Beatriz Monteiro <biafigmonteiro@gmail.com>
2025-11-05 22:34:13 -0300

    fix auth to deploy

--- src/router/guards/auth.guards.ts
+++ src/router/guards/auth.guards.ts
@@ -1,7 +1,7 @@
 import type { NavigationGuardNext, RouteLocationNormalizedGeneric, RouteLocationNormalizedLoadedGeneric } from "vue-router"
 import { getStoredToken } from "../../utils/auth.storage"
 
-export function guardsTo(to: RouteLocationNormalizedGeneric, from: RouteLocationNormalizedLoadedGeneric, next: NavigationGuardNext): void {
+export function guardsTo(to: RouteLocationNormalizedGeneric, _from: RouteLocationNormalizedLoadedGeneric, next: NavigationGuardNext): void {
     const isAuth = getStoredToken()
 
     if (to.name !== 'login' && !isAuth) {
