e8bab2e46a2613e1fdb90b004b1a91aaa4e4fd88
Beatriz Monteiro <biafigmonteiro@gmail.com>
2025-11-06 00:31:00 -0300

    fix config rxjs

--- src/service/api-Config/rxjs.config.ts
+++ src/service/api-Config/rxjs.config.ts
@@ -6,11 +6,10 @@ const get = <T>(
     url: string,
     params?: object,
     baseURL?: string,
-    responseType: ResponseType = "json",
-    token?: string
+    responseType: ResponseType = "json"
 ): Observable<T> => {
     return defer(() =>
-        api(baseURL, token).get<T>(url, { params, responseType })
+        api(baseURL).get<T>(url, { params, responseType })
     ).pipe(map((result) => result.data));
 };
 
@@ -18,10 +17,9 @@ const post = <T>(
     url: string,
     body: object,
     params?: object,
-    baseURL?: string,
-    token?: string
+    baseURL?: string
 ): Observable<T | void> => {
-    return defer(() => api(baseURL, token).post<T>(url, body, { params })).pipe(
+    return defer(() => api(baseURL).post<T>(url, body, { params })).pipe(
         map((result) => result.data)
     );
 };
@@ -30,10 +28,9 @@ const put = <T>(
     url: string,
     body: object,
     params?: object,
-    baseURL?: string,
-    token?: string
+    baseURL?: string
 ): Observable<T | void> => {
-    return defer(() => api(baseURL, token).put<T>(url, body, { params })).pipe(
+    return defer(() => api(baseURL).put<T>(url, body, { params })).pipe(
         map((result) => result.data)
     );
 };
@@ -42,21 +39,20 @@ const patch = <T>(
     url: string,
     baseURL: string,
     body?: object,
-    params?: object,
-    token?: string
+    params?: object
 ): Observable<T | void> => {
-    return defer(() => api(baseURL, token).patch<T>(url, body, { params })).pipe(
+    return defer(() => api(baseURL).patch<T>(url, body, { params })).pipe(
         map((result) => result.data)
     );
 };
 
 const deleteR = <T>(
     url: string,
-    baseURL?: string,
-    token?: string
+    baseURL?: string
 ): Observable<T | void> => {
-    return defer(() => api(baseURL, token).delete(`${url}`)).pipe(
+    return defer(() => api(baseURL).delete(`${url}`)).pipe(
         map((result) => result.data)
     );
 };
+
 export default { get, post, put, patch, deleteR };
